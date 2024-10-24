3dad8f66fa67eb4d2aeb6cacac6cc906f00dbc29
md82680 <mdespo@gmail.com>
2024-10-24 11:26:02 -0400

    style changes to home.module.scss

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,16 +23,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/dessert": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/dessert.js"
-    ],
-    "/dinner": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/dinner.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fbb16e9c8421deb3"; }
+/******/ 		__webpack_require__.h = function() { return "36eb64ad7cb36d9d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
